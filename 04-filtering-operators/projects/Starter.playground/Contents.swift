cbcc09ba4f74527afc8b9112cc231829d79de4ce
Tung Vu <tungvuduc2805.apple@gmail.com>
2021-08-29 10:21:22 +0700

    filtering operation

--- 04-filtering-operators/projects/Starter.playground/Contents.swift
+++ 04-filtering-operators/projects/Starter.playground/Contents.swift
@@ -3,32 +3,47 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+//example(of: "Filter") {
+//    let numbers = (1...10).publisher
+//
+//    numbers
+//        .filter { $0 % 3 == 0}
+//        .map {
+//            "\($0) is multiple of 3"
+//        }
+//        .sink(receiveValue: {
+//            print($0)
+//        })
+//        .store(in: &subscriptions)
+//}
 
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+example(of: "Compact Map") {
+    let strings = ["a", "1.24", "3", "def", "5"].publisher
+    
+    // hard-core developer
+//    strings
+//        .map { Float($0) }
+//        .filter { $0 != nil }
+//        .compactMap {$0}
+//        .sink(receiveValue: {
+//            print($0)
+//        })
+    
+    // "Work smart - not hard"
+    strings
+        .compactMap { Float($0) }
+        .sink(receiveValue: {
+            print($0)
+        })
+}
+
+example(of: "Challenge") {
+    let collection = (1...100).publisher
+    
+    collection
+        .dropFirst(50)
+        .prefix(20)
+        .filter { $0 % 2 == 0}
+        .sink(receiveValue: { print($0) })
+    
+}
